c14e112a6f7f985647aabddf7490b44e0d5ba9c3
CoderDojo Den Haag Ninja <coderdojo.denhaag@gmail.com>
2022-09-18 15:30:58 +0200

    update

---
+++ 18-09-2022/versie 1.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.speed(100)
+for i in range (11):
+    turtle.bgcolor("firebrick")
+    turtle.pensize(50)
+    turtle.color("gold")
+    turtle.forward(50)
+    turtle.bgcolor("lightgreen")
+    turtle.color("aqua")
+    turtle.forward(50)
+    turtle.color("gold")
+    turtle.left(360/11)
+
